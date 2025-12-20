a134d1f6b88e4d9fe52d64e87589036aa366a6ba
Aditi181123 <tanuaditi04@gmail.com>
2025-12-20 14:39:42 +0530

    [Aditi Mittal] Add. LeetCode 5 Questions

---
+++ core-java-practice/leetcode-codebase/ispalindrome.java
@@ -0,0 +1,26 @@
+package leetcode_codebase;
+import java.util.*;
+
+public class ispalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter  a  number: ");
+        int n = sc.nextInt();
+
+        int a = n ;
+        int r = 0;
+
+        while (n != 0) {
+            int d = n % 10;
+            r = r * 10 + d;
+            n = n / 10;
+        }
+
+        if (a == r) {
+            System.out.println("Palindrome number");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+    }
+}
---
+++ core-java-practice/leetcode-codebase/twosum.java
@@ -0,0 +1,14 @@
+package leetcode_codebase;
+
+import java.util.*;
+
+public class twosum{
+	public static void main(System[] args){
+		System.out.println("enter two number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b  = sc.nextInt();
+        int sum = a + b ;
+        System.out.println("sum : " + sum );
+    }
+}
