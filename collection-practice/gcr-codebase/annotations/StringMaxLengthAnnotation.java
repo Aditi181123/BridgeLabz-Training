3daff2b5c0784ab962daa951021e4cbe43429779
Aditi181123 <tanuaditi04@gmail.com>
2026-01-16 16:58:27 +0530

    [Aditi Mittal] Add Annotations

---
+++ collection-practice/gcr-codebase/annotations/StringMaxLengthAnnotation.java
@@ -0,0 +1,49 @@
+package annotations;
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface MaxLength{
+	int value();
+}
+
+class User{
+
+	@MaxLength(value = 6)
+	String username;
+
+	public User(String username) {
+		validate(username);
+		this.username = username;
+	}
+
+	public void validate(String username) {
+		try {
+			Field field = this.getClass().getDeclaredField("username");
+			if(field.isAnnotationPresent(MaxLength.class)) {
+				MaxLength len = field.getAnnotation(MaxLength.class);
+				if(username.length() > len.value()) {
+					throw new IllegalArgumentException (
+							"User Name exceeds max length of: " + len.value());
+				}
+				else {
+					System.out.println("Length of Username: " + username.length());
+				}
+				
+			}
+		}
+		catch(NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+public class StringMaxLengthAnnotation {
+	public static void main(String[] args){
+		new User("Aditi");
+		new User("NoSuchFieldException");
+		
+	}
+}
+
